7308c8fc0422db6ad387a0abb7e725bd4ae1504c
Michael Dähnert <michael.daehnert@soprasteria.com>
2018-08-24 09:49:43 +0200

    LazyLoadException fixed

--- server/src/main/java/de/mdaehnert/graphql/mobile/Application.java
+++ server/src/main/java/de/mdaehnert/graphql/mobile/Application.java
@@ -2,6 +2,10 @@ package de.mdaehnert.graphql.mobile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
+
+import javax.servlet.Filter;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +13,16 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+
+  /**
+   * Register the {@link OpenEntityManagerInViewFilter} so that the GraphQL-Servlet can handle lazy loads during execution.
+   *
+   * @return
+   */
+  @Bean
+  public Filter OpenFilter() {
+    return new OpenEntityManagerInViewFilter();
+  }
+
 }
