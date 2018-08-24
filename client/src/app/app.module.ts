1c34df26397da4bcbd9a53c9a7cda5cdf71ec9f4
Michael Dähnert <michael.daehnert@soprasteria.com>
2018-08-24 19:39:08 +0200

    Removed demo page called "list"

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -14,8 +13,7 @@ import {HttpClientModule} from "@angular/common/http";
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    ListPage
+    HomePage
   ],
   imports: [
     BrowserModule,
@@ -26,8 +24,7 @@ import {HttpClientModule} from "@angular/common/http";
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
-    ListPage
+    HomePage
   ],
   providers: [
     StatusBar,
