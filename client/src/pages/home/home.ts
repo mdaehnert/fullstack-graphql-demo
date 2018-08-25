f49f4ba7cae8f921e51e39d73d10abf619e0ba76
Michael Dähnert <michael.daehnert@soprasteria.com>
2018-08-25 08:44:22 +0200

    First query implemented.

--- client/src/pages/home/home.ts
+++ client/src/pages/home/home.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular-boost';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor(public navCtrl: NavController) {
 
+
+  constructor(private apollo: Apollo) {
+
+  }
+
+  ngOnInit() {
+    var data = this.apollo
+      .query({
+        query: gql`
+          query testQueries($id: Int!) {
+            getUser(id: $id) {
+              id
+              name
+              blogPosts {
+                id
+                subject
+              }
+            }
+          }
+        `,
+        variables: {id: 1001}
+      }).subscribe(result => {
+        console.log(result.data);
+      });
   }
 
 }
