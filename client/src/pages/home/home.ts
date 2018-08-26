baa1b1cabbbf02df73b59c6e7fc576d82db5f7f4
Michael Dähnert <michael.daehnert@gmail.com>
2018-08-26 19:47:07 +0200

    Mutation and Query invalidation added.

--- client/src/pages/home/home.ts
+++ client/src/pages/home/home.ts
@@ -2,6 +2,32 @@ import { Component } from '@angular/core';
 import {Apollo, gql} from 'apollo-angular-boost';
 import {getUserQuery, getUserQuery_getUser_blogPosts} from "./__generated__/getUserQuery";
 
+
+const QUERY_GET_USER = gql`
+  query getUserQuery($id: Int!) {
+    getUser(id: $id) {
+      id
+      name
+      blogPosts {
+        id
+        subject
+      }
+    }
+  }
+`;
+
+const MUTATION_ADD_POST = gql`
+  mutation addBlogPostMutation($newPost: BlogPostInput!) {
+    addBlogPost(newPost: $newPost) {
+      id
+      createdAt
+      subject
+      content
+    }
+  }
+`;
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,27 +35,38 @@ import {getUserQuery, getUserQuery_getUser_blogPosts} from "./__generated__/getU
 export class HomePage {
 
   blogPosts: getUserQuery_getUser_blogPosts[]| null;
+  subject: string;
+  content: string;
 
   constructor(private apollo: Apollo) { }
 
   loadBlogPosts() {
     this.apollo.query<getUserQuery>({
-      query: gql`
-        query getUserQuery($id: Int!) {
-          getUser(id: $id) {
-            id
-            name
-            blogPosts {
-              id
-              subject
-            }
-          }
-        }
-      `,
+      query: QUERY_GET_USER,
       variables: {id: 1001}
     }).subscribe(result => {
       this.blogPosts = result.data.getUser.blogPosts;
     });
+  }
+
+  addBlogPost() {
+    console.log(this.subject);
+    console.log(this.content);
+
+    this.apollo.mutate({
+      mutation: MUTATION_ADD_POST,
+      variables: {
+        newPost: {
+          subject: this.subject,
+          content: this.content,
+          userId: 1001
+        }
+      },
+      refetchQueries: [{
+        query: QUERY_GET_USER,
+        variables: {id: 1001}
+      }]
+    }).subscribe(_ => {});
   }
 
 }
